f97e5d83d4ac62fd821454339f5c75d8572be7cf
geimaj <16000925@vcconnect.co.za>
2020-01-16 13:44:18 +0200

    render selected colour in details section

--- src/ColourManager.js
+++ src/ColourManager.js
@@ -1,6 +1,14 @@
 import Colour from "./Colour";
 import QuantityControls from "./QuantityControls";
 import renderTargets from "./render-targets";
+import $ from "jquery"; //:(
+
+//element references
+const btnQuantityAccept = document.querySelector("#quantity-accept");
+const btnQuantityCancel = document.querySelector("#quantity-cancel");
+const btnAddToCart = document.querySelector("#addToCart");
+const productCount = document.querySelector("#productCount");
+const details = document.querySelector("#details");
 
 export default class ColourManager {
 	constructor(colourNames) {
@@ -8,24 +16,63 @@ export default class ColourManager {
 		this.randomColours = this.getRandomColours();
 		this.colourObjects = this.getColourObjects();
 		this.selectedColour = this.colourObjects[0];
-		this.quantity = 0;
+
+		this.state = {
+			quantity: 0
+		};
+
+		//bindings
+		this.incrementQuantity = this.incrementQuantity.bind(this);
+		this.decrementQuantity = this.decrementQuantity.bind(this);
+		this.quantityAccepted = this.quantityAccepted.bind(this);
+		this.quantityCanceled = this.quantityCanceled.bind(this);
+		this.setState = this.setState.bind(this);
+
+		//event listeners
+		btnQuantityAccept.onclick = this.quantityAccepted;
+		btnQuantityCancel.onclick = this.quantityCanceled;
 
 		//simulate a click during load to have default selectedColour
 		this.colourClicked(this.colourObjects[0]);
-
-		this.incrementQuantity = this.incrementQuantity.bind(this);
-		this.decrementQuantity = this.decrementQuantity.bind(this);
 	}
 
-	incrementQuantity() {
-		this.quantity = this.quantity + 1;
+	setState(state) {
+		this.state = { ...this.state, ...state };
 		this.render(renderTargets.colourManager);
 	}
 
+	quantityAccepted() {
+		console.log("accept");
+		btnAddToCart.innerText = "Checkout now";
+		productCount.innerText = this.state.quantity;
+		//hide modal
+		$("#modal").modal("hide");
+
+		//render selectedColour this.state.quantity times
+		details.innerHTML = "";
+		const colour = this.selectedColour;
+		colour.selected = false;
+		const colours = new Array(this.state.quantity).fill(colour);
+		details.appendChild(this.renderColourList(colours));
+	}
+
+	quantityCanceled() {
+		this.setState({
+			quantity: 0
+		});
+	}
+
+	incrementQuantity() {
+		this.setState({
+			quantity: this.state.quantity + 1
+		});
+	}
+
 	decrementQuantity() {
-		if (this.quantity > 0) {
-			this.quantity = this.quantity - 1;
-			this.render(renderTargets.colourManager);
+		if (this.state.quantity > 0) {
+			this.setState({
+				quantity: this.state.quantity - 1
+			});
 		}
 	}
 
@@ -59,16 +106,22 @@ export default class ColourManager {
 		this.selectedColour = colour;
 		this.selectedColour.selected = true;
 		//reset quantity
-		this.quantity = 0;
+		this.setState({
+			quantity: 0
+		});
+	}
 
-		this.render(renderTargets.colourManager);
+	renderColourList(colours) {
+		const colourElements = colours.map(el => el.render());
+		const list = document.createElement("ul");
+		list.append(...colourElements);
+		return list;
 	}
 
 	render(where) {
 		//render colours
-		const colourElements = this.colourObjects.map(el => el.render());
-		const list = document.createElement("ul");
-		list.append(...colourElements);
+		const list = this.renderColourList(this.colourObjects);
+
 		where.innerHTML = "";
 		where.appendChild(list);
 
@@ -77,7 +130,7 @@ export default class ColourManager {
 		q.innerHTML = "";
 		q.appendChild(
 			new QuantityControls(
-				this.quantity,
+				this.state.quantity,
 				this.incrementQuantity,
 				this.decrementQuantity
 			).render()
