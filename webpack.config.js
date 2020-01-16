ff3e7bf5e0e4d588dabd2cd70436d84ab0295777
geimaj <16000925@vcconnect.co.za>
2020-01-16 17:22:02 +0200

    enable HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,42 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
-    module: {
-        rules: [{
-            test: /\.s[ac]ss$/i,
-            use: [
-                // Creates `style` nodes from JS strings
-                'style-loader',
-                // Translates CSS into CommonJS
-                'css-loader',
-                // Compiles Sass to CSS
-                'sass-loader',
-            ],
-        }, ],
-    },
-}
+module.exports = env => {
+	console.log("NODE_ENV", env.NODE_ENV);
+	return {
+		mode: "development",
+		entry: "./src/index.js",
+		output: {
+			filename: "main.js",
+			path: path.resolve(__dirname, "dist")
+		},
+		devServer: {
+			contentBase: "./dist"
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "./src/index.html"
+			}),
+			new MiniCssExtractPlugin()
+		],
+		module: {
+			rules: [
+				{
+					test: /\.(sa|sc|c)ss$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+							options: {
+								hmr: env.NODE_ENV === "development",
+								reloadAll: true
+							}
+						},
+						"css-loader",
+						"sass-loader"
+					]
+				}
+			]
+		}
+	};
+};
