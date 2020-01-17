5b69afbf4134a14a325760bb1600e0417a7c34b4
geimaj <16000925@vcconnect.co.za>
2020-01-17 20:26:34 +0200

    add server start script

---
+++ start.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT || 8080;
+const distDir = `${__dirname}/dist/`;
+const app = express();
+app.use(express.static(distDir));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(distDir, "index.html"));
+});
+
+app.listen(port);
